d5c1a81cc751750097d43de6ecb67182b011cbfb
viktormoskalev <neonchilkk@gmail.com>
2020-02-17 14:02:17 +0200

    lol

--- javascript/swiper.js
+++ javascript/swiper.js
@@ -2,20 +2,23 @@
       slidesPerView: 1,
       spaceBetween: 10,
       loop: true,
-      // init: false,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
       breakpoints: {
         640: {
           slidesPerView: 1,
           spaceBetween: 20,
         },
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 40,
+        640:{
+          slidesPerView: 2,
+          spaceBetween: 30,
         },
+        1048: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },        
         1440: {
           slidesPerView: 4,
           spaceBetween: 50,
